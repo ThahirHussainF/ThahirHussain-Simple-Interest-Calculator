58418c2146c44c62da7e62b43dc653d810279970
ThahirHussainF <thahirhussain1414@gmail.com>
2021-12-01 11:17:53 +0000

    Changes made by F.Thahir Hussain

--- script.js
+++ script.js
@@ -1,6 +1,17 @@
 function compute()
 {
-    p = document.getElementById("principal").value;
-    
+    //p->principal ,r->interest rate, y->years
+    var p = document.getElementById("principal").value;
+    if(p<=0) {
+        alert("Enter positive number");
+        window.focus();
+        return false;
+    }
+    var r = document.getElementById("rate").value;
+    var y = document.getElementById("years").value;
+    var amount=p * y * r/100;
+    var year = new Date().getFullYear()+parseInt(y);
+    document.getElementById("result").innerHTML="If you deposit "+p+",\<br\>at an interest rate of "+r+"%\<br\>You will receive an amount of "+amount+",\<br\>in the year "+year+"\<br\>"
 }
+
         
